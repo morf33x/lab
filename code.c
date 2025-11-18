6aa580defd2b3c31eb19993d16e1c010675c9a12
Хусяинов Имиль ТКИ-142 <imilh@mail.ru>
2025-11-18 12:05:32 +0300

    Update code.c

--- code.c
+++ code.c
@@ -12,23 +12,6 @@ enum { JAN = 1, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC };
  */
 int getValue();
 
-
-/**
- * @brief проверяет,что переменная является одним из вариантов
- * @param value значение проверяемой переменной
- */
-void checkValue(const double value);
-
-/**
- * @brief проверяет,что переменная является днем недели
- * @param value значение проверяемой переменной
- */
-void checkValueW(const double value);
-/**
- * @brief проверяет,что переменная является номеров месяца
- * @param value значение проверяемой переменной
- */
-void checkValueM(const double value);
 /**
 * @brief точка входа вв  программу
 * @return 
@@ -38,19 +21,17 @@ int main()
     setlocale(LC_ALL, "rus");
     printf("Выберите:\n %d - день недели\n %d - месяц\n", WEEK, MONTH);
     const int chs = getValue();
-    checkValue(chs);
     switch (chs) 
     {
     case WEEK:
         printf("введите день недели:");
         const int day = getValue();
-        checkValueW(day);
         switch (day)
         {
-        case MON:
+        case WEEK:
             printf("Понедельник");
             break;
-        case TUE:
+        case MONTH:
             printf("Вторник");
             break;
         case WEN:
@@ -68,33 +49,35 @@ int main()
         case SUN:
             printf("Воскресенье");
             break;
+        default:
+            printf("Значение должно быть номером дня недели\n");
+            abort();
         }
         break;
     case MONTH:
         printf("введите номер месяца:");
         const int mth = getValue();
-        checkValueM(mth);
         switch (mth)
         {
-        case JAN:
+        case WEEK:
             printf("Январь");
             break;
-        case FEB:
+        case MONTH:
             printf("Февраль");
             break;
-        case MAR:
+        case WEN:
             printf("Март");
             break;
-        case APR:
+        case THU:
             printf("Апрель");
             break;
-        case MAY:
+        case FRI:
             printf("Май");
             break;
-        case JUN:
+        case SAT:
             printf("Июнь");
             break;
-        case JUL:
+        case SUN:
             printf("Июль");
             break;
         case AUG:
@@ -112,7 +95,14 @@ int main()
         case DEC:
             printf("Декабрь");
             break;
+        default:
+            printf("Значение должно быть номером месяца\n");
+            abort();
+            break;
         }
+    default:
+        printf("Неправильный выбор\n");
+        abort();
     }
 }
 
@@ -125,29 +115,4 @@ int getValue()
         abort();
     }
     return value;
-}
-
-void checkValue(const double value)
-{
-    if (value <= 0 || value > 2)
-    {
-        printf("Неправильный выбор\n");
-        abort();
-    }
-}
-void checkValueW(const double value)
-{
-    if (value <= 0 || value > 7)
-    {
-        printf("Значение должно быть номером дня недели\n");
-        abort();
-    }
-}
-void checkValueM(const double value)
-{
-    if (value <= 0 || value > 12)
-    {
-        printf("Значение должно быть номером месяца\n");
-        abort();
-    }
 }
