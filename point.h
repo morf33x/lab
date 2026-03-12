4510978c74f14cf055f47eaf6a5953edef3926bd
Хусяинов Имиль ТКИ-142 <imilh@mail.ru>
2026-03-12 20:54:41 +0300

    Update point.h

--- point.h
+++ point.h
@@ -2,12 +2,35 @@
 class point
 {
 private:
+    /**
+    * @brief x - координата точки по оси OX
+    */
 	double x;
+    /**
+    * @brief y - координата точки по оси OY
+    */
 	double y;
 public:
+     /**
+    * @brief конструктор, создает точку, с координатами x,y
+    * @param x - координата точки по оси OX
+    * @param y - координата точки по оси OY
+    */
 	point(const double x1 = 0, const double y1 = 0);
+     /**
+    * @brief оператор проверяет совпадают ли точки
+    */
 	bool operator==(point p) const;
+  /**
+    * @brief получение координаты x
+    * @return x
+    */
 	double getX() const;
+    /**
+    * @brief получение координаты y
+    * @return y
+    */
 	double getY() const;
 };
+
 
