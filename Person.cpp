3a5af478767039666d7b85db21729e741c6c2930
Хусяинов Имиль ТКИ-142 <imilh@mail.ru>
2026-05-08 17:43:24 +0300

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,9 +1,12 @@
-﻿#include "Person.h"
+#include "Person.h"
 
-Person::Person() : firstName(""), lastName(""), patronymic(""), prefix(""), postfix("") {}
+int Person::counter = 1;
+
+Person::Person() : id(counter++), firstName(""), lastName(""), patronymic(""), prefix(""), postfix("") {}
 
 Person::Person(const std::string& firstName, const std::string& lastName,
     const std::string& patronymic, const std::string& prefix, const std::string& postfix)
+    : id(counter++)
 {
     this->firstName = firstName;
     this->lastName = lastName;
@@ -12,12 +15,18 @@ Person::Person(const std::string& firstName, const std::string& lastName,
     this->postfix = postfix;
 }
 
+int Person::getId() const
+{
+    return id;
+}
+
 std::ostream& operator<<(std::ostream& os, const Person& person)
 {
     if (!person.prefix.empty()) os << person.prefix << " ";
     os << person.lastName << " " << (!person.firstName.empty() ? person.firstName.substr(0, 1) + "." : "");
     if (!person.patronymic.empty()) os << person.patronymic.substr(0, 1) << ".";
     if (!person.postfix.empty()) os << " " << person.postfix;
+
     return os;
 }
 
